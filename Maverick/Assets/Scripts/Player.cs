f6178346cb40c76ea7204f2326b0860fb9dd89f6
TheAvianLord <angela-yixuan21@hotmail.com>
2017-07-14 19:58:23 -0700

    PlayerMovement

---
+++ Maverick/Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public float x_mov;
+    public float y_mov;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        x_mov = Input.GetAxis("Horizontal");
+        y_mov = Input.GetAxis("Vertical");
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(x_mov * speed, y_mov * speed);
+    }
+
+}
